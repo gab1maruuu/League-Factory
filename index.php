70191a6252635550e449ba30adb3acb4aecd13be
nicolasalvo <nicolasoxosal@campus.monlau.com>
2025-12-16 16:39:14 +0100

    Refactor index.php and header.php for improved readability and structure

--- index.php
+++ index.php
@@ -2,18 +2,38 @@
 session_start();
 require 'config/Database.php';
 $action = $_GET['action'] ?? 'posts';
-switch($action){
-  case 'posts': require 'controllers/PostController.php'; (new PostController())->index(); break;
-  case 'login': require 'controllers/UserController.php'; (new UserController())->login(); break;
+switch ($action) {
+  case 'posts':
+    require 'controllers/PostController.php';
+    (new PostController())->index();
+    break;
+  case 'login':
+    require 'controllers/UserController.php';
+    (new UserController())->login();
+    break;
   // añade más rutas: show_post, post_create, comment_store, admin_...
-  default: echo "Ruta no encontrada";
+  default:
+    echo "Ruta no encontrada";
 }
 
-include __DIR__ ."/views/layout/header.php"
+include __DIR__ . "/views/layout/header.php"
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
 
-<?php 
-include __DIR__ ."/views/layout/footer.php"
+<body>
+  <h2 class="text-center">League Factory</h2>
+</body>
+
+</html>
+<?php
+include __DIR__ . "/views/layout/footer.php"
 ?>
--- views/layout/header.php
+++ views/layout/header.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <h1>wadadwda</h1>
+    <h1 class="py-4 text-xl ml-4">League Factory</h1>
 </body>
 </html>
