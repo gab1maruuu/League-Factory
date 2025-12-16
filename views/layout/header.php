210e80c49931f1bfa68a5bb9e933c098227b4d3e
nicolasalvo <nicolasoxosal@campus.monlau.com>
2025-12-16 17:03:55 +0100

    Enhance header layout with gradient background and additional navigation links

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body>
-    <h1 class="py-4 text-xl ml-4">League Factory</h1>
+    <div class="bg-gradient-to-r from-zinc-400 to-zinc-700 py-4">
+        <header class="flex items-center justify-between ml-4 ">
+            <div>
+                <h1 class="text-xl font-bold ">
+                    League Factory
+                </h1>
+            </div>
+            <nav>
+                <ul class="text-md font-bold flex flex-row gap-4 justify-end mr-4">
+                    <li><a href="index.php?action=posts" class="text-white-500 hover:underline">Inicio</a></li>
+                    <li><a href="index.php?action=login" class="text-white-500 hover:underline">Crea tu Equipo</a></li>
+                    <li><a href="index.php?action=login" class="text-white-500 hover:underline">Unirse a una Liga</a></li>
+                    <li><a href="index.php?action=login" class="text-white-500 hover:underline">Clasificacion</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+
 </body>
+
 </html>
