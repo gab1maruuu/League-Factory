6db5e63598e24bd5472a97913c15c4226d0f2dad
Tommy <tommyfigjim@campus.monlau.com>
2025-12-18 18:11:22 +0100

    header

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 session_start();
 require 'config/Database.php';
 $action = $_GET['action'] ?? 'posts';
+switch($action){
+  case 'posts': require 'controllers/PostController.php'; (new PostController())->index(); break;
+  case 'login': require 'controllers/UserController.php'; (new UserController())->login(); break;
+  // añade más rutas: show_post, post_create, comment_store, admin_...
+  default: echo "Ruta no encontrada";
+}
+
+
+include __DIR__ ."/views/layout/header.php";
+include __DIR__ ."/views/layout/inicio.php";
 ?>
 
-<div class="min-h-screen flex flex-col">
-  <?php
-  switch($action){
-    case 'posts': require 'controllers/PostController.php'; (new PostController())->index(); break;
-    case 'login': require 'controllers/UserController.php'; (new UserController())->login(); break;
-    default: echo "Ruta no encontrada";
-  }
 
-  include __DIR__ ."/views/layout/header.php";
-  include __DIR__ ."/views/layout/inicio.php";
-  ?>
-</div>
 
 <?php 
 include __DIR__ ."/views/layout/footer.php"
