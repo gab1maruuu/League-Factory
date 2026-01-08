f556e2d710f8f49f0dc3ac5d595e64dd0e4226ae
nicolasalvo <nicolasoxosal@campus.monlau.com>
2026-01-08 19:07:51 +0100

    UserController

---
+++ controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'models/User.php';
+class UserController {
+  private $userModel;
+  public function __construct(){ $pdo=(new Database())->getPdo(); $this->userModel = new User($pdo); }
+  public function login(){
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $user = $this->userModel->findByEmail($email);
+    if($user && password_verify($password, $user['password_hash'])){
+      $_SESSION['user_id'] = $user['id'];
+      $_SESSION['role'] = $user['role'];
+      header("Location: index.php?action=posts");
+    } else {
+      header("Location: index.php?action=login&error=1");
+    }
+  }
+}
+?>
