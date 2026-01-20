7fc475c038ab155c12de1fe1f533b694697298b1
T00MMYYY <tommyfigueroajimenez@gmail.com>
2026-01-20 12:35:41 +0100

    cambioImagen

--- views/user/profileUser.php
+++ views/user/profileUser.php
@@ -3,13 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Perfil - League Factory</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <main class="max-w-4xl px-6 py-12">
+    <main class="max-w-4xl px-6 py-12">   
         <section class="flex items-center mb-10">
-                <img src="/public/images/perfil.png" class="h-35 w-40 opacity-80" alt="Foto de perfil"> 
+            <form action="/controllers/update_photo.php" method="POST" enctype="multipart/form-data" id="form-avatar">
+                <label for="input-file" class="cursor-pointer">
+                    <img src="/public/images/perfil.png" class="h-35 w-40 opacity-80 hover:opacity-60 transition duration-300" alt="Foto de perfil">
+                    <input type="file" name="avatar" id="input-file" class="hidden" accept="image/*" onchange="document.getElementById('form-avatar').submit()">
+                </label>
+            </form>
+
             <div>
                 <h1 class="text-3xl font-bold tracking-tight mb-1">
                     <?php echo __("profile") ?>
@@ -21,14 +27,34 @@
         </section>
 
         <nav class="flex border-b border-gray-800 mb-8 space-x-8">
-            <a href="#" class="pb-4 text-sm font-bold border-b-2 border-indigo-600 text-white"><?php echo __('settings') ?></a>
-            <a href="#" class="pb-4 text-sm font-bold text-gray-400 hover:text-white transition duration-300"><?php echo __('account') ?></a>
-            <a href="#" class="pb-4 text-sm font-bold text-gray-400 hover:text-white transition duration-300"><?php echo __('estadisticas') ?></a>
+            <a href="#" class="pb-4 text-sm font-bold border-b-2 border-indigo-600 text-white">
+                <?php echo __('settings') ?>
+            </a>
+            <a href="#" class="pb-4 text-sm font-bold text-gray-400 hover:text-white transition duration-300">
+                <?php echo __('account') ?>
+            </a>
+            <a href="#" class="pb-4 text-sm font-bold text-gray-400 hover:text-white transition duration-300">
+                <?php echo __('estadisticas') ?>
+            </a>
         </nav>
 
         <section>
-            <h2 class="text-sm font-bold mb-4 uppercase tracking-wider text-gray-300"><?php echo __('Account setup') ?></h2>
-        </section>
+            <h2 class="text-sm font-bold mb-4 uppercase tracking-wider text-gray-300">
+                <?php echo __('Account setup') ?>
+            </h2>
+            </section>
+
     </main>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
